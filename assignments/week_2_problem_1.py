90cc40c1797dd1f207f8112c102d1735b9b5f795
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-08 17:56:44 +0000

    push

--- assignments/week_2_problem_1.py
+++ assignments/week_2_problem_1.py
@@ -1,21 +1,21 @@
 def main():
     d = deep_to_main(input("What is the Answer to the Great Question of Life,"))
 
-def deep_to_main(d):
+def deep_to_main(d): # goes through 3 ways 42 can be a string
 
-    if d == ("42"):
+    if d == ("42"): 
 
         return print("Yes")
-    elif d == ("forty-two") or ("forty-two".upper()):
+    elif d == ("forty-two") or ("forty-two".upper()): #the or statment allows for any case type .upper forces the string to be upper case
         
         
         return print("Yes")
-    elif d == ("forty two") or ("forty two".upper()):
+    elif d == ("forty two") or ("forty two".upper()): #the or statment allows for any case type .upper forces the string to be upper case
 
         return print("Yes")
-    else:
+    else: #if any other case is input the function will return 0
 
-        return print("no")
+        return print("no")  
         
 
 main()
