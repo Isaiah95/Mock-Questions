b04f0f82ee6c45da0cd6f86a7ab2dfa54ecf87dd
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-08 17:46:55 +0000

    push

---
+++ assignments/week_2_problem_1.py
@@ -0,0 +1,23 @@
+def main():
+    d = deep_to_main(input("What is Answer to the Great Question of Life,"))
+
+def deep_to_main(d):
+
+    if d == ("42"):
+
+        return print("yes")
+    elif d == ("forty-two") == d("forty-two").upper():
+        
+        return print("yes")
+    elif d == d.lower() == ("forty two"):
+
+        return print("yes")
+    else:
+
+        return print("no")
+        
+
+main()
+    
+
+
