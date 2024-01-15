3229a5f4b76bbfb3d7d6ed2dae8a88625805a9e2
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-15 04:24:37 +0000

    push

---
+++ assignments/week_2_problem_4.py
@@ -0,0 +1,15 @@
+def main():
+
+    x, y, z = expression.split((input("Expression: ")))
+
+
+
+    print()
+
+        
+
+def  expression(x, y , z):
+        
+        if y == "+":
+
+            return x + z
