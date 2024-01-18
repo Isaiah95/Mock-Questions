f648b3e21e4ee0977d4c290d7bd2736429457a6f
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-18 18:20:39 +0000

    push

---
+++ assignments/week_3_problem_1.py
@@ -0,0 +1,18 @@
+def main():
+    name(input("camelCase: "))
+
+def name(n):
+
+    for n.istitle() in n:
+
+        n.split(n.title)
+
+        i = len
+
+        replace(n.istitle(), '_'+n[i])
+
+        n = n.lower()
+
+
+        print("_"+)
+    
