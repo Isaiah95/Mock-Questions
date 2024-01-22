ae551bd2051aaa33e12cd016e4cdc2b673628b2c
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-22 16:47:08 +0000

    push

--- assignments/week_3_problem_1.py
+++ assignments/week_3_problem_1.py
@@ -1,18 +1,21 @@
 def main():
-    name(input("camelCase: "))
+    names(input("camelCase: ")) # gets the user input for the string
 
-def name(n):
-
-    for n.istitle() in n:
-
-        n.split(n.title)
-
-        i = len
-
-        replace(n.istitle(), '_'+n[i])
-
-        n = n.lower()
+def names(n):
+    s= "" #strings are immutable, so you would need a new empty string to replace the uppercase letters with "_"
 
 
-        print("_"+)
+    for i in n:
+
+        if(i.isupper()): #checks to see if anything in the string is uppercase
+            s+="_" # if so then it will replace them "_"
+            s += i.lower() # Also the lower case letter instead of the upper one
+        else:    
+            s+= i.lower() # it nothing if found that it upper case it will finish going through the rest of the string
+            
+             
+    print("snake_case:",s)
+
+if __name__ == "__main__": # proper heading for main
+    main()       
     
