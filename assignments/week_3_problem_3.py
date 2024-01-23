0ee2709fea34ad944176681310d0377288afcf28
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-23 18:58:46 +0000

    push

--- assignments/week_3_problem_3.py
+++ assignments/week_3_problem_3.py
@@ -1,9 +1,13 @@
 def twtrr():
-    t = input("Input: ")
-    s = ['a', 'e', 'i', 'o', 'u']
-    d = "".join([s if in t else t])
-        
-    print("Output:", t.replace(s,""))
+    t = input("Input: ") # Gets user input
+    t = t.lower() # forces everthing to be lower
+    s = ['a', 'i','e', 'o','u'] # list of all vowels
+    for d in s:  # checks to see if a vowel is used 
+        if d in t: #replaces the vowels with an empty char
+            t = t.replace(d,'')
+    print("Output:", t)
+
+
 
 
 
