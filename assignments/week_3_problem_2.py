1312c7c3488d257d731c70c99f8b1e160de9b84f
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-23 16:07:05 +0000

    push

--- assignments/week_3_problem_2.py
+++ assignments/week_3_problem_2.py
@@ -1,14 +1,22 @@
-def main():
-    coin(input("Insert Coin: "))
-
-def coin(c):
-        c = int(c)
-        if c != 50 and c ==5 or c == 10 or c == 25:
-              c = 50 - c
-              print ("Amount due: ",c)
+def coin():
+        d = 50 # The total number of coins need to buy a soda
+        s=[5,10,25] # an array of all the possible types of coins you could use
+        
+        while d > 0:  #a while loop is used to keep asking for coin until the 50 is payed
             
-              if c <=0:
-                print("change Owed: ", abs(c))
-              
-if __name__ == "__main__": # proper heading for main
-    main()       
+            c=int(input("Insert Coin: " )) # takes the coin from user and stores it as c
+
+            if c in s : # if c is in the array of s it will subtract your coin from 50 then store d as that new value 
+                
+                d = d - c
+                
+            if d > 0:
+                 print ("Amount due: ",d) # its nesscary to use the order here so that it wouldn't ask for amount due after the change is 0
+
+            elif d <= 0:
+                
+                print("Change Owed: ", abs(d))
+
+                
+           
+coin()   
