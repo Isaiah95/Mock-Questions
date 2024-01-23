26a3ff33fc4db100d6ab334b000b2de2ece2d575
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-23 17:05:02 +0000

    push

---
+++ assignments/week_3_problem_3.py
@@ -0,0 +1,16 @@
+def twtrr():
+    t = input("Input: ")
+    s = ['a', 'e', 'i', 'o', 'u']
+    d = "".join([s if in t else t])
+        
+    print("Output:", t.replace(s,""))
+
+
+
+
+
+
+
+
+
+twtrr()
