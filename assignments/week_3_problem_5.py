4502792048e477eb8274c4c0f68676749dd9f3e1
Isaiah95 <47825885+Isaiah95@users.noreply.github.com>
2024-01-24 00:42:09 +0000

    push

--- assignments/week_3_problem_5.py
+++ assignments/week_3_problem_5.py
@@ -1,5 +1,27 @@
-thisdict = {
+thisdict = { #used a dictnoary to make values and keys for all the furits
   "apple": "130",
   "avacoado": "50",
-  "banana": 110
+  "banana": "110",
+  "cantaloupe" : "50",
+  "grapefruit" : "60",
+  "grapes" : "60",
+  "honeydew" : "50",
+  "kiwifruit" : "50",
+  "lemon" : "15",
+  "lime" : "20",
+  "nectarine" : "60",
+  "orange" : "80",
+  "peach" : "60",
+  "pear" : "100",
+  "pineapple" : "50",
+  "plums" : "70",
+  "strawberries" : "50",
+  "sweetcherries" : "100",
+  "tangerine" : "50",
+  "watermelon" : "80"  
 }
+item = input("Item: ") # gets the users item as a string
+item = item.lower() #converts any string into all lowercase
+if item in thisdict: #prints the item Calories if its in the dictonary
+    
+  print("Calories: ", thisdict[item]) 
